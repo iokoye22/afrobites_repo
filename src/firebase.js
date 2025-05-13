3f7ee31759ed94379ab45b20a821003df7063808
iokoye22 <iokoye22@bu.edu>
2025-05-13 15:45:20 -0400

    fire base and git connected

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1lHvalfzIxSPJv86gNH151VPD5ipI5aw",
+  authDomain: "afrobites-ai.firebaseapp.com",
+  projectId: "afrobites-ai",
+  storageBucket: "afrobites-ai.firebasestorage.app",
+  messagingSenderId: "1046524359500",
+  appId: "1:1046524359500:web:1302a3fab8c5761ce815e9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
